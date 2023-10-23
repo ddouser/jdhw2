16a659b233f905ba201f6b950b9e877a1d272f7a
ddouser <44279659+ddouser@users.noreply.github.com>
2023-10-23 17:24:06 +0700

    111

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.SimpleTimeZone;
 import java.util.SplittableRandom;
-
+//
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
